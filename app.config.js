eae071271134b3116178baa5364c262fc9e85660
SakuraSedaia <jcburger2003@outlook.com>
2025-12-27 05:18:18 -0600

    Fix typo in `app.config.js`: correct `statis` to `static` and add missing comma

--- app.config.js
+++ app.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   server: {
-    baseURL: process.env.BASE_URL
-    statis:true
+    baseURL: process.env.BASE_URL,
+    static: true,
     prerender: {
       failOnError: true,
       routes: ["/"],
-      crawlLinks: true
-    }
-  }
+      crawlLinks: true,
+    },
+  },
 });
